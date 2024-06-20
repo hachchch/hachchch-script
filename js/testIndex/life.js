146cb833fdcd1fe03b627e593fd4321335e11bdf
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-20 14:09:21 +0900

    Update and rename main.js to life.js

---
+++ js/testIndex/life.js
@@ -0,0 +1,15 @@
+public class life { 
+        public static void live(String[] args) {
+                //クラスのオブジェクトを生成
+                eukaryota c = new eukaryota();
+        
+                //メンバ変数を参照する
+                String str1 = c.name;
+        
+                //メソッドの呼出し
+                String str2 = c.testMethod();
+        
+                //結果を出力
+                System.out.println(str1 + str2);
+        }
+}
