224e786e4ed087f6dfc038405f19ec79848e88dc
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-20 14:13:36 +0900

    Update eukaryota.js

--- js/testIndex/eukaryota.js
+++ js/testIndex/eukaryota.js
@@ -1,7 +1,12 @@
 class eukaryota {
         // メンバ変数
         public String name = "Eukaryote";
-    
+
+        // 引数ありのコンストラクタ
+        public eukaryota(String name) {
+                this.name = name;
+        }
+
         // メソッド
         public String testMethod(){
                 return "Multicellular";
