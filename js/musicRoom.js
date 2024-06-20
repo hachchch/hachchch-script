ef1561df7bf1c24be7ec0988fc3259518e05a08b
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-21 00:58:25 +0900

    Update musicRoom.js

--- js/musicRoom.js
+++ js/musicRoom.js
@@ -1,3 +1,19 @@
+function pressTrigger() {
+                document.getElementById('PressSound').pause();
+                document.getElementById('PressSound').currentTime = 0;
+                press();
+            }
+                function press() {
+                    document.getElementById("PressSound").play();
+                }
+            function selectTrigger() {
+                document.getElementById('SelectSound').pause();
+                document.getElementById('SelectSound').currentTime = 0;
+                select();
+            }
+                function select() {
+                    document.getElementById("SelectSound").play();
+                }
 function GAbgmRestart() {
           document.getElementById('GA').pause();
           document.getElementById('GA').currentTime = 0;
