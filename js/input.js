c43a37c697939e148fdd2933b1ed3c6873ceea97
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-20 22:03:36 +0900

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,3 +1,22 @@
+// 効果音
+function ringTrigger() {
+    document.getElementById('ClickSound').pause();
+    document.getElementById('ClickSound').currentTime = 0;
+    ring();
+}
+function ring() {
+   document.getElementById("ClickSound").play();
+}
+
+function pressTrigger() {
+    document.getElementById('PressSound').pause();
+    document.getElementById('PressSound').currentTime = 0;
+    press();
+}
+function press() {
+   document.getElementById("PressSound").play();
+}
+// ボタン
 const res = document.getElementById("commandResult");
 
           function input(value) {
@@ -25,6 +44,7 @@ const res = document.getElementById("commandResult");
                   // alert("下");
               }
               res.value = resultInput.substring(0, res.value.length -1);
+              pressTrigger();
               if (res.value.length < 1){
                   stopRepeatTimer();
               }
@@ -36,8 +56,63 @@ const res = document.getElementById("commandResult");
                       nIntervId = setInterval(startMovement, 250);
                   }
                 }
-              function stopRepeatTimer() {
-              clearInterval(nIntervId);
-              // 変数から intervalID を解放
-              nIntervId = null;
-              }
+function stopRepeatTimer() {
+        clearInterval(nIntervId);
+        // 変数から intervalID を解放
+        nIntervId = null;
+}
+// キーボード
+window.addEventListener(
+        "keydown",
+        (event) => {
+                if (event.defaultPrevented) {
+                        return;
+                }
+                switch (event.key) {
+                        case "ArrowDown":
+                                liveScreen('↓');
+                                ringTrigger();
+                                break;
+                        case "ArrowUp":
+                                liveScreen('↑');
+                                ringTrigger();
+                                break;
+                        case "ArrowLeft":
+                                liveScreen('←');
+                                ringTrigger();
+                                break;
+                        case "ArrowRight":
+                                liveScreen('→');
+                                ringTrigger();
+                                break;
+                        case "s":
+                                liveScreen('↓');
+                                ringTrigger();
+                                break;
+                        case "w":
+                                liveScreen('↑');
+                                ringTrigger();
+                                break;
+                        case "a":
+                                liveScreen('←');
+                                ringTrigger();
+                                break;
+                        case "d":
+                                liveScreen('→');
+                                ringTrigger();
+                                break;
+                        case "Enter":
+                                repeatTimer();
+                                ringTrigger();
+                                break;
+                        case "Backspace":
+                                res.value = resultInput.substring(0, res.value.length -1);
+                                ringTrigger();
+                                break;
+                        default:
+                                return;
+                }
+                event.preventDefault();
+        },
+        true,
+);
