27bce90758eabb2beb55056bfd69dec171dc910a
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-20 14:17:41 +0900

    Update life.js

--- js/testIndex/life.js
+++ js/testIndex/life.js
@@ -7,7 +7,7 @@ public class life {
                 String str1 = c.name;
         
                 //メソッドの呼出し
-                String str2 = c.testMethod();
+                String str2 = c.eukaryoticCell();
         
                 //結果を出力
                 System.out.println(str1 + str2);
